270e284a0df19be792307ed18ccb74947ce3061e
Annie Huang <annie.hn@gmail.com>
2023-08-09 14:44:56 +1000

    !!!! Set up formula to slowly faded .top-section background color until it scroll to 30% of for the current page, after it reaches 30%, totally remove the background color

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ function setScrollVar() {
   const percentOfScreenHeightScrolled =
     htmlElement.scrollTop / htmlElement.clientHeight;
 
+  // console.log('htmlElement.scrollTop=', htmlElement.scrollTop);
+  // console.log('htmlElement.clientHeight=', htmlElement.clientHeight);
+
   console.log(Math.min(percentOfScreenHeightScrolled * 100, 100));
   // It will never get more than 100, if it scroll to the next page, we will just put '100' into --scroll
   htmlElement.style.setProperty(
